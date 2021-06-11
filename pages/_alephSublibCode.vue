7d38d455610446cb2f6aa5c2a9b727fcd2c24277
murugadas7 <35003571+murugadas7@users.noreply.github.com>
2021-06-11 19:02:31 +0530

    Update _alephSublibCode.vue

--- pages/_alephSublibCode.vue
+++ pages/_alephSublibCode.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="ui container">
-    <template>
+     
       <div class="ui text menu">
         <div class="item">
           <NuxtLink to="/">
@@ -14,7 +14,7 @@
             
         </div>
       </div>
-    </template>
+     
     <h2 class="ui header">
       <div class="ui grid">
         <div class="sixteen wide column"></div>
@@ -240,4 +240,37 @@ export default {
     display: none;
   }
 }
+
+
+.bounce-enter-active {
+  transform-origin: top;
+  animation: bounce-in 0.8s;
+}
+.bounce-leave-active {
+  transform-origin: top;
+  animation: bounce-out 0.5s;
+}
+@keyframes bounce-in {
+  0% {
+    transform: scale(0);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes bounce-out {
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.25);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+
 </style>
